208da3abc87c6b83dae5c915c37fa497483284ca
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-02-11 16:12:59 -0700

    Initial commit

---
+++ src/routes/5/+page.svelte
@@ -0,0 +1,6 @@
+<enhanced:img
+	class="absolute inset-0 -z-10 hidden h-dvh object-cover md:block"
+	src="$lib/rsvp-desktop.jpg"
+	alt="Us in front of a lake at a couples' retreat. November, 2024."
+	sizes="100vw"
+/>
