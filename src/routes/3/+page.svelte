ca3f9029c8b57a71def2384935ad13bee0587d5a
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-02-11 16:19:27 -0700

    easier repro

--- src/routes/3/+page.svelte
+++
@@ -1,6 +0,0 @@
-<enhanced:img
-	class="absolute inset-0 -z-10 hidden h-dvh object-cover md:block"
-	src="$lib/rsvp-desktop.jpg"
-	alt="Us in front of a lake at a couples' retreat. November, 2024."
-	sizes="100vw"
-/>
